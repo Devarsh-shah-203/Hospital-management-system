82c766c1e88e6b47351e5d8bfd4cd7386659980a
Devarsh-shah-203 <devarshshah203@gmail.com>
2026-07-11 03:04:30 +0530

    added confirmation email of appointmnet

--- backend/src/controllers/appointment.controller.js
+++ backend/src/controllers/appointment.controller.js
@@ -1,17 +1,18 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import { createAppointmentService,getDoctorAvailabilityService,getMyAppointmentsService,cancelAppointmentService } from "../services/appointment.service.js";
-import { createQueueEntryService} from "../services/queue.service.js";
-
+import {
+  createAppointmentService,
+  getDoctorAvailabilityService,
+  getMyAppointmentsService,
+  cancelAppointmentService,
+} from "../services/appointment.service.js";
+import { createQueueEntryService } from "../services/queue.service.js";
+import { appointmentConfirmationEmail } from "../services/email.service.js";
 
 export const createAppointment = asyncHandler(async (req, res) => {
   const patientId = req.user.id;
 
-  const {
-    doctorId,
-    appointmentDate,
-    appointmentTime,
-  } = req.body;
+  const { doctorId, appointmentDate, appointmentTime } = req.body;
 
   const appointment = await createAppointmentService({
     patientId,
@@ -22,65 +23,93 @@ export const createAppointment = asyncHandler(async (req, res) => {
 
   const queue = await createQueueEntryService(appointment._id);
 
-  return res.status(201).json(
-    new ApiResponse(
-      201,
-      appointment,
-      queue,
-      "Appointment booked successfully"
+  await appointment.populate([
+    {
+      path: "patient",
+      select: "username email",
+    },
+    {
+      path: "doctor",
+      select: "username",
+    },
+  ]);
+
+  console.log(
+    JSON.stringify(
+      {
+        appointment,
+        patient: appointment.patient,
+        doctor: appointment.doctor,
+        queue,
+      },
+      null,
+      2
     )
   );
+
+  await appointmentConfirmationEmail({
+    email: appointment.patient.email,
+    name: appointment.patient.username,
+    date: appointment.appointmentDate,
+    time: appointment.appointmentTime,
+    doctorName: appointment.doctor.username,
+    queueNo: queue.queueNumber,
+    queuePos: queue.position,
+  });
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        201,
+        appointment,
+        queue,
+        "Appointment booked successfully",
+      ),
+    );
 });
 
 export const getMyAppointments = asyncHandler(async (req, res) => {
   const appointments = await getMyAppointmentsService(req.user.id);
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      appointments,
-      "Appointments fetched successfully"
-    )
-  );
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, appointments, "Appointments fetched successfully"),
+    );
 });
 
 export const cancelAppointment = asyncHandler(async (req, res) => {
   const appointment = await cancelAppointmentService(
     req.params.appointmentId,
-    req.user.id
+    req.user.id,
   );
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      appointment,
-      "Appointment cancelled successfully"
-    )
-  );
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, appointment, "Appointment cancelled successfully"),
+    );
 });
 
 export const getDoctorAvailability = asyncHandler(async (req, res) => {
-  const  doctorId  = req.params.doctorId;
+  const doctorId = req.params.doctorId;
   const { date } = req.query;
 
-  console.log("Doctor Id:",doctorId)
+  console.log("Doctor Id:", doctorId);
   if (!date) {
-    return res.status(400).json(
-      new ApiResponse(400, null, "Date is required")
-    );
+    return res.status(400).json(new ApiResponse(400, null, "Date is required"));
   }
 
-  const availability = await getDoctorAvailabilityService(
-    doctorId,
-    date
-  );
+  const availability = await getDoctorAvailabilityService(doctorId, date);
 
-  return res.status(200).json(
-    new ApiResponse(
-      200,
-      availability,
-      "Doctor availability fetched successfully"
-    )
-  );
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        availability,
+        "Doctor availability fetched successfully",
+      ),
+    );
 });
-
